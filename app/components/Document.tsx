e65c6d10abdcbe74d523fccb29c900b3d7dfe9e2
Nasir Uddin <nasir.rubel99@gmail.com>
2025-03-23 12:31:30 +0600

    travelpayouts varification

--- app/components/Document.tsx
+++ app/components/Document.tsx
@@ -1,6 +1,12 @@
 /** @format */
 
-import { Links, LiveReload, ScrollRestoration, Scripts, Meta } from "@remix-run/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 import type { FC, PropsWithChildren } from "react";
 import { useShouldHydrate } from "remix-utils";
 
@@ -8,10 +14,23 @@ export const Document: FC<PropsWithChildren> = ({ children }) => {
   let shouldHydrate = useShouldHydrate();
 
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <Meta />
         <Links />
+        <script
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          dangerouslySetInnerHTML={{
+            __html: `(function () {
+    var script = document.createElement("script");
+    script.async = 1;
+    script.src = 'https://tp-em.com/NDAxMjM5.js?t=401239';
+    document.head.appendChild(script);
+  })();`,
+          }}
+        />
       </head>
       <body className={`flex min-h-screen mw-100vw flex-col`}>
         {children}
