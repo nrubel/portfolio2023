8347c98c73908974c03472194d7b06077d521630
Nasir <nasir.rubel99@gmail.com>
2026-04-30 17:01:40 +0600

    Remove unused development files and type definitions for Next.js cache and routing

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mRqWMI+c6xfmzXVWowJ78dPfYd++GTQwhFoMgJwm73E=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0hhthdn._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0r29rby._.js","static/chunks/0idx_next_0wiaqoo._.js","static/chunks/02qi_zod_v4_03vdj1u._.js","static/chunks/02qi_zod_v3_0fhybq-._.js","static/chunks/0qs5_@ai-sdk_gateway_dist_index_mjs_0m0qs.e._.js","static/chunks/0v04_ai_dist_index_mjs_10bv4si._.js","static/chunks/0w-y_swr_dist_0zajues._.js","static/chunks/node_modules__pnpm_0zttwae._.js"],
-    source: "dynamic"
-});
